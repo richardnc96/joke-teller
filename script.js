400708706e4def95519ff72375bbc04262f2eb7b
Richard Nathaniel Chandra <richardnc96@gmail.com>
2021-01-29 21:02:55 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+const button = document.getElementById('button');
+const audioElement = document.getElementById('audio');
+
+
+
+// Disable/Enable Button
+function toggleButton(){
+    button.disabled = !button.disabled;
+}
+
+//Passing Joke to VoiceRSS API
+function tellMe(joke){
+    
+    VoiceRSS.speech({
+        key: '6797f479d2514ef9978437d0e4653f74',
+        src: joke,
+        hl: 'en-us',
+        v: 'Linda',
+        r: 0, 
+        c: 'mp3',
+        f: '44khz_16bit_stereo',
+        ssml: false
+    });
+}
+
+// Get Jokes from Joke API
+async function getJokes(){
+    let joke = '';
+    const apiUrl = 'https://sv443.net/jokeapi/v2/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist';
+    try{
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if (data.setup){
+            joke = `${data.setup} ... ${data.delivery}`;
+        } else{
+            joke = data.joke;
+        }
+        // Text-to-Speech
+        tellMe(joke);
+        // Disable Button
+        toggleButton();
+    } catch(error){
+        // Catch Errors Here
+        console.log('whoops', error)
+    }
+}
+
+// Event Listeners
+button.addEventListener('click', getJokes);
+audioElement.addEventListener('ended', toggleButton);
